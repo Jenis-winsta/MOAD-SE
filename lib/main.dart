0061be3ea1de325b2fdac6fad82a769fb24622ae
RobinRajesh <123891319+RobinRajesh@users.noreply.github.com>
2023-02-20 11:42:57 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:finalapp/LoginScreen/login.dart';
-// import 'package:finalapp/User/user.dart';
-// import 'package:finalapp/Admin/admin.dart';
+import 'dart:async';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +9,73 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // home: Admin(),
-      home: Login(),
-      debugShowCheckedModeBanner: false,
-
+    return MaterialApp(
+      home: SplashScreen(),
     );
   }
 }
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> Homepage()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.jpeg', height: 400),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.purple),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text("Title of the app"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Homepage"),
+        )
+      ),
+    );
+  }
+}
