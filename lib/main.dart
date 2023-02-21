ee133acb2e1caa79605e69ca7c3410b0f4497083
Jenis-winsta <jeniswinsta@gmail.com>
2023-02-21 06:53:10 +0530

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:finalapp/LoginScreen/login.dart';
+import 'package:finalapp/splashScreen.dart';
 // import 'package:finalapp/User/user.dart';
 // import 'package:finalapp/Admin/admin.dart';
 import 'package:flutter/material.dart';
@@ -9,45 +10,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       // home: Admin(),
-      home: Login(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 3), (){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> Homepage()));
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/logo.jpeg', height: 400),
-            SizedBox(height: 20),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.purple),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
 
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
+// SafeArea(child: Login())
